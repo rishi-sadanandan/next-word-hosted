25def8f05ab5dc73d373fae90d3f932229a319ab
Rishikesh Sadanandan <rishikeshsadanandan@Rishikeshs-MacBook-Pro-2.local>
2022-11-11 20:09:00 -0600

    pushed api and function resources

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "nextwordFunction": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "nextword": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
